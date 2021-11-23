0b4f219b672dce08385be94638982013fda5e4f2
lavanon <35270586+lavanon@users.noreply.github.com>
2021-11-23 19:36:59 +0000

    update the view, data is parsing in.

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,96 @@
-
-
 <script>
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    
-  }
-}
-
-
-// async function search(query, days) {
-// //     if (query.test(/\W/g)) {
-// //         return console.log("please");
-// //     }
-//     if (query === null || query === undefined) {
-//         return console.log("please enter something a little less hacky");
-//     }
-//     if (!Number(days)) {
-//         return console.log("please enter a numbers")
-//     }
-
-//     let response = await
-//     fetch(`https://api.parsely.com/v2/search?apikey=arstechnica.com&q=${query}&days=${days}`)
-//     let data = await response.json()
-//     return data;
-// }
-
-// search("apple", 1).then((data)=> console.log(data)) // output will be the required data
-
-
-//JS
-//Consume and show thew api data
-  // going use the vue for render method to show the titles
-  // using the h1, show the title 
-
-
-// let dataset = {};
-
-// dataset.map(data => {
-//     title: data.Title;
-//     image: data.image;
-//     description: data.Tags;
-//     publishDate: data.pushblishDate;
-//     breadcrumb: data.parsely;
-// })
-
-Vue.createApp({
+    // HelloWorld
+  },
   data() {
     return {
-      items: [{ message: 'Foo' }, { message: 'Bar' }]
+      apiData: []
     }
+  },
+  mounted() {
+    fetch("https://api.parsely.com/v2/search?apikey=arstechnica.com&q=apple&days=30")
+      .then(res => res.json())
+      .then(data => this.apiData = data.data)
   }
-}).mount('#array-rendering')
+}
 </script>
 
-
 <template>
-  <li id="array-rendering">
-  <!-- <li v-for="item in items" :key="item.message"> -->
-    {{ items }}
-  <!-- </li> -->
-</li>
+<div id="searchButton" class="ui fluid action input">
+  <input type="text" placeholder="Search...">
+  <div class="ui button">Search</div>
+</div>
+
+<div class="ui comments" v-for="(item, index) in apiData" :key="index">
+  <div class="comment">
+    <a class="avatar">
+      <img id="img" :src="item.image_url" >
+    </a>
+    <div class="content">
+      <a class="author">Tom Lukic</a>
+      <div class="text">
+        This will be great for business reports. I will definitely download this.
+      </div>
+      <div class="actions">
+        <a class="reply">Reply</a>
+        <a class="save">Save</a>
+        <a class="hide">Hide</a>
+        <a>
+          <i class="expand icon"></i>
+          Full-screen
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+  <!-- <div id="app" class="ui comments" v-for="(item, index) in apiData" :key="index">
+    <a class="avatar">
+      <img id="img" :src="item.image_url" >
+    </a>
+    <div class="content">
+      <a class="author">{{item.title}}</a>
+      <div class="metadata">
+        <span class="date">{{item.pub_date}}</span>
+      </div>
+      <div class="text">
+        {{item.author}}
+      </div>
+      <div class="actions">
+        <a class="reply">Reply</a>
+      </div>
+    </div>
+  </div> -->
+<!-- <div id="app" v-for="(item, index) in apiData" :key="index">
+  <ul id="item" >
+    <li><img id="img" :src="item.image_url" ></li>
+    
+    <li>{{index}}. {{item.title}}</li>
+    <li>{{item.pub_date}} </li>
+    </ul>
+</div> -->
 </template>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* #app {
+    padding: 0.5rem;
+    background: #f8f9fa;
+    color: #495057;
+    border: 1px solid #dee2e6;
+    border-radius: 6px;
+}
+#item {
+      display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+} */
+#img {
+  height: 30px;
+  width:30px;
+}
+#searchButton {
+  padding-bottom: 10px;
 }
 </style>
