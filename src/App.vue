7071562e4c2e91f432455b500ee3033a76b496a8
lavanon <35270586+lavanon@users.noreply.github.com>
2021-11-24 12:06:49 +0000

    add css for random classes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
-<script>
 <script>    
     import NavBar from './components/NavBar.vue'
 
     export default {
         name: 'app',
-        components: { },
         components: { NavBar},
         data() {
             return {
             search: 'tesla',
             apiData: [],
+            selected: 30
             }
         },
         mounted() {
@@ -23,13 +22,15 @@
                 .then(res => res.json())
                 .then(data => this.apiData = data.data)
             }
-        }
+        },
     }
+
 </script>
 
 
 <template>
  <NavBar/>
+ <h3 class="ui headers ">Search for Articles</h3>
     <div id="searchButton" class="ui fluid action input">
         <input type="text" v-on:keyup.enter="activateSearch" v-model="search" placeholder="Search...">
         <div @click="activateSearch" class="ui button">Search</div>
@@ -41,30 +42,40 @@
             <option value="365">This Year</option>
         </select>
     </div>
-    <div id="article" class="ui comments" v-for="(item, index) in apiData" :key="index">
-        <div class="comment">
-            <a class="avatar">
+     <h3 class="ui headers margin-bottom">Search Results</h3>
+
+    <div id="article" 
+        v-for="(item, index) in apiData" 
+        :key="index"
+        >
+        <div>
+            <a  style=" vertical-align: top; display: table-cell;">
                 <img id="img" :src="item.image_url" >
             </a>
-            <div class="content">
-                <a :href="item.url" target="blank" class="author">{{item.title}}</a>
-                <div class="metadata">
-                    <span class="date">{{item.pub_date}}</span>
-                </div>
+            <div style="vertical-align: top; display: table-cell;">
+                <div class="ui label">{{item.section}}</div>
+                <h4 class="headerMargin"><a :href="item.url" target="blank" class="author">{{item.title}}</a></h4>
                 <div class="text">
-                    {{item.author}}
+                   <strong> By {{item.author}} </strong> | {{item.pub_date.split("T")[0]}}
                 </div>
             </div>
         </div>
+        <hr class="hrPadding">
+
     </div>
 </template>
 
 
-
 <style>
+    .headers{
+        margin-bottom: 0;
+        padding-left: 20px;
+    }
     #img {
-        height: 30px;
-        width:30px;
+        height: 100px;
+        width:140px;
+        margin-right: 20px;
+        object-fit: cover;
     }
     #searchButton {
         padding: 20px;
@@ -72,5 +83,19 @@
     #article {
         padding-left: 20px;
         max-width:750px;
+    }
+    .dropdown {
+        border: 1px solid lightgray;
+        margin-left: 10px;
+    }
+
+    .headerMargin {
+        margin-top: 10px;
+    }
+    .hrPadding{
+        margin: 20px 0;
+    }
+    .margin-bottom {
+        margin-bottom: 20px;
     }
 </style>
